bba281c130c4064e74b3d8fcd2e9c4c26a4309ae
Vinai Phomsouvannady <93491285+VinaiPhomsouvannady@users.noreply.github.com>
2022-12-02 23:18:27 -0500

    implemented tic-tac-toe following different tutorial

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,17 @@
 // pages/404.js
 import { useState } from 'react';
-import Game from '../components/game';
+import Link from 'next/link';
+import utilStyles from '../styles/utils.module.css';
+
 
 
 
 export default function Custom404() {
-    return <body><h1>Nice to meet you!</h1>
-    <Game />
+    return <body><h1 className={utilStyles.heading2Xl}>Nice to meet you!</h1>
+    <h1 className={utilStyles.heading2Xl}>404!!!!</h1>
+            <div className={utilStyles.heading2Xl}>
+        <Link href="/game"> Want to play a game?</Link>
+        </div>
     </body>
     ;
   }
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { getSortedPostsData } from '../lib/posts';
 import Date from '../components/date';
 
+
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
