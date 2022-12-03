b40d5e37cc30d3aa92aed7916d3cf3828c34b451
Vinai Phomsouvannady <93491285+VinaiPhomsouvannady@users.noreply.github.com>
2022-12-02 21:34:54 -0500

    ok

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,17 @@
 // pages/404.js
+import { useState } from 'react';
+import Game from '../components/game';
+
+
+
 export default function Custom404() {
-    return <body><h1>Nice to meet you!</h1></body>;
+    return <body><h1>Nice to meet you!</h1>
+    <Game />
+    </body>
+    ;
   }
+
+  
+
+
   
